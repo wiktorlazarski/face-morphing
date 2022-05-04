8bf639c47a3b3b95eb0788dacfcae37fe2e4c54b
AnetaJas <jas.aneta@wp.pl>
2022-05-04 12:45:16 +0200

    poprawki

--- scripts/training/data_loading.py
+++ scripts/training/data_loading.py
@@ -12,16 +12,16 @@ class HelenFaceMorphingDataset(torchvision.datasets.VisionDataset):
         super().__init__(root=dataset_root)
 
         self.images_path = self._load_images()
-        pass
+        self.keypoints_df = pd.read_csv(Path(self.root) / "metadata.csv")
 
     def __len__(self) -> int:
         return len(self.images_path)
 
     def __getitem__(self, index: int) -> t.Tuple[torch.Tensor, torch.Tensor]:
         image_path = self.images_path[index]
-        image, key_points = self._load_sample(image_path)
+        image, keypoints = self._load_sample(image_path)
 
-        return image, key_points
+        return image, keypoints
 
     def _load_images(self) -> t.List[str]:
         images_dir = Path(self.root) / "images"
@@ -33,11 +33,10 @@ class HelenFaceMorphingDataset(torchvision.datasets.VisionDataset):
     def _load_sample(self, image_path: Path) -> t.Tuple[str, torch.Tensor]:
         image = cv2.imread(str(image_path))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        key_points_csv = pd.read_csv(image_path.parent.parent / "metadata.csv")
 
-        key_points = key_points_csv.loc[
-            key_points_csv["Filenames"] == image_path.name.rsplit(".", 1)[0]
+        keypoints = self.keypoints_df.loc[
+            self.keypoints_df["Filenames"] == image_path.name.rsplit(".", 1)[0]
         ]
-        key_points = key_points.iloc[0]
+        keypoints = keypoints.iloc[0]
 
-        return image, key_points
+        return image, keypoints
